823f22438048bdd2a99a2bb2ba5cad40c1950873
neerajnerlekar <neeraj.nerlekar@gmail.com>
2022-12-26 06:03:48 -0700

    update README, add Navbar.js to components, add lensContants to store the authentication, add LensContext to add context like MoralisProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar'
+import { MoralisProvider } from 'react-moralis'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider initializeOnMount={false}>
+     <Navbar />
+     <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
